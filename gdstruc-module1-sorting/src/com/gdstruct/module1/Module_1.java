ffaed451d21b51b72aaf51be0403cd3fcfeca4ac
TralaleroTralala1-3 <oppaidaisuki162@gmail.com>
2025-11-29 17:14:54 +0800

    Quiz 1

---
+++ gdstruc-module1-sorting/src/com/gdstruct/module1/Module_1.java
@@ -0,0 +1,28 @@
+package com.gdstruct.module1;
+
+public class Module_1 {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int[] numbers = new int[10];
+
+        numbers[0] = 35;
+        numbers[1] = 69;
+        numbers[2] = 1;
+        numbers[3] = 10;
+        numbers[4] = -50;
+        numbers[5] = 320;
+        numbers[6] = 63;
+        numbers[7] = 58;
+        numbers[8] = 26;
+        numbers[9] = 13;
+    }
+    private static void printArrayElements(int[] arr)
+    {
+        for (int j : arr)
+        {
+            System.out.println(j);
+        }
+    }
+}
