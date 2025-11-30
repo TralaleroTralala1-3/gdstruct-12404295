738350e4dc688a707f72b33de60f8857b0383f67
TralaleroTralala1-3 <oppaidaisuki162@gmail.com>
2025-11-30 11:55:25 +0800

    Quiz 2

---
+++ gstruc-module2-lists/src/com/gdstruct/module2/player.java
@@ -0,0 +1,47 @@
+package com.gdstruct.module2;
+
+public class player
+{
+    private int id;
+    private String name;  // <-- Capital S
+    private int level;
+
+    public player(int id, String name, int level) {  // <-- Capital S
+        this.id = id;
+        this.name = name;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {  // <-- Capital S
+        return name;
+    }
+
+    public void setName(String name) {  // <-- Capital S
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "player{" +
+                "id=" + id +
+                ", name=" + name +
+                ", level=" + level +
+                '}';
+    }
+}
